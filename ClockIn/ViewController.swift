f2543208eb2c0aa9e6939b160da3d14ce132ebb1
iptkrisna <krisnamath9@gmail.com>
2019-07-04 19:25:14 +0700

    fix user default

--- ClockIn/ViewController.swift
+++ ClockIn/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var clockButton: UIButton!
+    @IBOutlet weak var label: UILabel!
+    var userDef = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let email = userDef.string(forKey: "email")
+        label.text = email
+        
     }
 
-
+    @IBAction func clockInTapped(_ sender: UIButton) {
+        userDef.set(emailTF.text, forKey: "email")
+        label.text = emailTF.text
+        
+    }
+    
 }
 
